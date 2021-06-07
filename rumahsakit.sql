fa18b2e27d581e033312602817120e1d047e4430
farhanntu29 <85468820+farhanntu29@users.noreply.github.com>
2021-06-07 10:34:07 +0700

    Add files via upload

--- rumahsakit.sql
+++ rumahsakit.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2020 at 04:58 AM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.1.24
+-- Generation Time: Jun 07, 2021 at 05:33 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,6 +34,13 @@ CREATE TABLE `tb_dokter` (
   `alamat` text NOT NULL,
   `no_telp` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_dokter`
+--
+
+INSERT INTO `tb_dokter` (`id_dokter`, `nama_dokter`, `spesialis`, `alamat`, `no_telp`) VALUES
+('9d4417be-8c25-47a1-805e-2a0a30e72cb9', 'Muhammad Farhan', 'Jantung', 'Cikoko', '085710316215');
 
 -- --------------------------------------------------------
 
@@ -76,6 +82,14 @@ CREATE TABLE `tb_pasien` (
   `no_telp` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tb_pasien`
+--
+
+INSERT INTO `tb_pasien` (`id_pasien`, `nomor_identitas`, `nama_pasien`, `jenis_kelamin`, `alamat`, `no_telp`) VALUES
+('8a2b3a84-4c2e-489a-8e78-6c43a6f5567e', '1218271827192819829', 'Udin', 'L', 'Cikoko Barat 2', '0212345678'),
+('bf1f8b6d-5496-4a63-9d30-d0857e843202', '3074082903950001', 'Muhammad Farhan', 'L', 'Cikoko', '085710316215');
+
 -- --------------------------------------------------------
 
 --
@@ -87,6 +101,60 @@ CREATE TABLE `tb_poliklinik` (
   `nama_poli` varchar(100) NOT NULL,
   `lokasi` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_poliklinik`
+--
+
+INSERT INTO `tb_poliklinik` (`id_poli`, `nama_poli`, `lokasi`) VALUES
+('953e0d15-5467-49e6-a30e-766963079a3d', 'gigi', 'jakarta'),
+('ae5035d2-9c95-4c7c-967e-98713ef3f616', 'test', 'test1'),
+('b37b9d88-309c-4020-84cd-0caf3a3f5c3d', 'jantung', 'bandung');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_rekammedis`
+--
+
+CREATE TABLE `tb_rekammedis` (
+  `uuid` int(11) NOT NULL,
+  `tgl` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `poli` varchar(128) CHARACTER SET latin1 DEFAULT NULL,
+  `pasien` varchar(128) CHARACTER SET latin1 DEFAULT NULL,
+  `keluhan` varchar(128) CHARACTER SET latin1 DEFAULT NULL,
+  `dokter` varchar(128) CHARACTER SET latin1 DEFAULT NULL,
+  `diagnosa` varchar(128) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_rekammedis`
+--
+
+INSERT INTO `tb_rekammedis` (`uuid`, `tgl`, `poli`, `pasien`, `keluhan`, `dokter`, `diagnosa`) VALUES
+(0, '2021-06-04 00:00:00.000000', 'ae5035d2-9c95-4c7c-967e-98713ef3f616', 'bf1f8b6d-5496-4a63-9d30-d0857e843202', '<p>pusing</p>\r\n', '9d4417be-8c25-47a1-805e-2a0a30e72cb9', 'corona'),
+(9, '2021-06-04 00:00:00.000000', 'ae5035d2-9c95-4c7c-967e-98713ef3f616', 'bf1f8b6d-5496-4a63-9d30-d0857e843202', '<p>pusing</p>\r\n', '9d4417be-8c25-47a1-805e-2a0a30e72cb9', 'corona'),
+(330, '2021-06-04 00:00:00.000000', 'ae5035d2-9c95-4c7c-967e-98713ef3f616', 'bf1f8b6d-5496-4a63-9d30-d0857e843202', '<p>pusing</p>\r\n', '9d4417be-8c25-47a1-805e-2a0a30e72cb9', 'corona'),
+(18692886, '2021-06-05 00:00:00.000000', 'ae5035d2-9c95-4c7c-967e-98713ef3f616', 'bf1f8b6d-5496-4a63-9d30-d0857e843202', '<p>sqxsq</p>\r\n', '9d4417be-8c25-47a1-805e-2a0a30e72cb9', 'qsxq');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_rm_obat`
+--
+
+CREATE TABLE `tb_rm_obat` (
+  `id` int(8) NOT NULL,
+  `id_rm` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `id_obat` varchar(50) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_rm_obat`
+--
+
+INSERT INTO `tb_rm_obat` (`id`, `id_rm`, `id_obat`) VALUES
+(0, '18692886-d2da-40d2-af9a-a24ef83db655', '26fed132-9b8c-11ea-a0ac-30d16bf3adce');
 
 -- --------------------------------------------------------
 
@@ -124,7 +192,8 @@ ALTER TABLE `tb_dokter`
 -- Indexes for table `tb_obat`
 --
 ALTER TABLE `tb_obat`
-  ADD PRIMARY KEY (`id_obat`);
+  ADD PRIMARY KEY (`id_obat`),
+  ADD KEY `id_obat_2` (`id_obat`);
 
 --
 -- Indexes for table `tb_pasien`
@@ -139,10 +208,40 @@ ALTER TABLE `tb_poliklinik`
   ADD PRIMARY KEY (`id_poli`);
 
 --
+-- Indexes for table `tb_rekammedis`
+--
+ALTER TABLE `tb_rekammedis`
+  ADD PRIMARY KEY (`uuid`),
+  ADD KEY `keluhan` (`keluhan`),
+  ADD KEY `tb_rekammedis_ibfk_2` (`diagnosa`),
+  ADD KEY `tb_rekammedis_ibfk_1` (`poli`),
+  ADD KEY `tb_rekammedis_ibfk_3` (`pasien`),
+  ADD KEY `dokter` (`dokter`);
+
+--
+-- Indexes for table `tb_rm_obat`
+--
+ALTER TABLE `tb_rm_obat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_obat` (`id_obat`);
+
+--
 -- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`id_user`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_rekammedis`
+--
+ALTER TABLE `tb_rekammedis`
+  ADD CONSTRAINT `tb_rekammedis_ibfk_1` FOREIGN KEY (`poli`) REFERENCES `tb_poliklinik` (`id_poli`),
+  ADD CONSTRAINT `tb_rekammedis_ibfk_3` FOREIGN KEY (`pasien`) REFERENCES `tb_pasien` (`id_pasien`),
+  ADD CONSTRAINT `tb_rekammedis_ibfk_4` FOREIGN KEY (`dokter`) REFERENCES `tb_dokter` (`id_dokter`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
